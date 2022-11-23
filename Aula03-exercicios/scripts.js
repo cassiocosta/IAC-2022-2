885db8ac22e98928ff28e9307a0a331d1d4f2724
Cássio h Costa <cassio@prouserapps.com>
2022-11-22 21:24:25 -0300

    correcao dos exercícios

---
+++ Aula03-exercicios/scripts.js
@@ -0,0 +1,65 @@
+
+
+function calcular(oper,n1,n2){
+    var result = 0;
+    
+    switch (oper) {
+        case "+":
+            result = n1+n2;
+            break;
+        case "-":
+            result = n1-n2;
+            break;
+        case "*":
+            result = n1*n2;
+            break;
+        case "/":
+            result = n1/n2;
+            break;
+        default:
+            return "somente as 4 operações arit.";
+    }
+    return result;
+}
+
+function tabuadaDo(numero){
+    console.log(`Tabuada do ${ numero }\n---------------`);
+
+    for (i = 0; i <= 10; i++) {
+        console.log(`${ numero } x ${ i } = ${ numero * i };`)
+    }
+}
+
+function imprimeFones(){
+    var phones = '519595959595,51848848448,988498766,959595959';
+    var arrPhones = phones.split(',');
+
+    for(i=0;i<arrPhones.length;i++){
+        console.log(`fone ${i+1}: ${arrPhones[i]}`)
+    }
+}
+
+function convertToDecimal(numero){
+
+    var arrNumero = numero.split("");
+    
+    var posicao = arrNumero.length -1;
+    var resultDeCadaAlgarimo = 0;
+    var resultFinal = 0;
+
+    for(i=0;i<arrNumero.length;i++){
+        console.log(`posicao ${i} = ${arrNumero[i]}`);
+        console.log(`posicao ${posicao} = ${arrNumero[i]}`);
+        //(a*b elevado a posiçao)
+        resultDeCadaAlgarimo = (arrNumero[i] * (2**posicao));
+        resultFinal = resultFinal + resultDeCadaAlgarimo;
+        posicao--;
+    }
+    return resultFinal;
+}
+
+// 0
+// 0<4
+// posicao 0 = 1
+// 1<4
+//posicao 1 = 0
