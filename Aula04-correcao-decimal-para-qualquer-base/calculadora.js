97a57458ecffa7231e0e4656c0f29a5908eb1f17
Cássio h Costa <cassio@prouserapps.com>
2022-11-29 21:51:52 -0300

    agora com o inverter

--- Aula04-correcao-decimal-para-qualquer-base/calculadora.js
+++ Aula04-correcao-decimal-para-qualquer-base/calculadora.js
@@ -14,41 +14,82 @@
 // 1 - pegar um número e divir por 2 (base)
 // 2 - armazenar esse número para divir novamente.
 
-function convertDecimalToBinary(numero){
+function convertDecimalToBinary(numero, base){
     var dividendo = numero;
     var restoDivisao = 0;
     var result = "";
     
     var i = 0;
     while (i == 0 ){
-        restoDivisao = dividendo%2;
-        console.log(`O resto da divisão de ${dividendo} é ${restoDivisao}`);
-        dividendo = parseInt(dividendo / 2) ;
-        console.log(`proximo dividendo ${dividendo}`);
+        restoDivisao = dividendo%base;
+        //console.log(`O resto da divisão de ${dividendo} é ${restoDivisao}`);
+        dividendo = parseInt(dividendo / base) ;
+        //console.log(`proximo dividendo ${dividendo}`);
         result +=restoDivisao;
         
-        if(dividendo<2){
+        if(dividendo<base){
             i++;
             result+=dividendo;
             
         }
-        console.log(`result concat ${result}`);
     }
+    return inverter(result);
 
-    
+
 }
+
+
 /**
 numero=10; 
 i=1;
 dividendo = 5;
 resto = 0;
 result = 01
+ */
 
+function inverter(numero){
+    var result = "";
+    var arrNumero = numero.split("");
+    for (i=arrNumero.length-1;  i>=0; i--) {
+        result+=arrNumero[i];
+            
+    }
+    return result;
 
+}
 
+/**
+ 
+numero 0101
+arrNum
+0   1   2   3
+0   1   0   1
 
+1 0 1 0
+
+i=3
+3==0
+result = 1010
 
  */
+function inverter2(num){
+    var arr = String(num).split('');
+    var arr2 = [];
+    for(var i = arr.length - 1; i >= 0; i--){
+        arr2.push(arr[i]);
+    }
+    return Number(arr2.join(''));
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
